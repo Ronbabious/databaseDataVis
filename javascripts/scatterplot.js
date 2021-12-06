8bd9e6fb2447263fa87552dd538135e491c7416d
Ronbabious <36154720+Ronbabious@users.noreply.github.com>
2021-12-06 16:52:35 +0100

    Added scatterplot back

--- javascripts/scatterplot.js
+++ javascripts/scatterplot.js
@@ -3,125 +3,401 @@ var scatterPlot = {
         "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
         "title": "USDA Food Database",
         "data": {
-        "url": "data/data.csv"
+        "url": "data/data.json"
         },      
         "transform": [
         {"filter": "datum.ONIscore > 0 && datum.F6 != '' && datum.Protein != 'NULL' && datum.Fat != 'NULL'&& datum.Carb != 'NULL' && datum.Calorie != 'NULL'"},
-        {"fold": ["ONIscore", "Protein"], "as": ["ONIscore", "val"]},
-        {"filter": {"selection": "kpi"}}
         ],
-        "layer": [
+        "concat": [
           {
-            "selection": {
-              "kpi": {
-                "type": "single",
-                "init": {"Choice": "country"},
-                "bind": {
-                  "Choice": {
-                    "name": "Ratings Per",
-                    "input": "select",
-                    "options": ["department", "country"]
-                  }
-                }
-              }
-            },
-          }],
-        //ScatterPlot # 1
-            //Encoding start
-            "mark": "circle",
+            //ScatterPlot # 1
             "encoding": {
-              //Legends
               "color": {
-                "condition": {
-                "param": "brush",
-                "title": "Food Categories",
-                "field": "F6",
-                "type": "nominal",
-                //Gray out non selection
-                },
-                "value": "gray"
+                  "condition": {
+                  "param": "brush",
+                  "title": "Food Categories",
+                  "field": "F6",
+                  "type": "nominal",
+    
+                  },
+                  "value": "gray"
+              },
+              "tooltip": [
+                  { "field": "foodName", "type": "nominal" },
+                  { "field": "Protein", "type": "nominal" },
+                  { "field": "Fat", "type": "nominal" },
+                  { "field": "Carb", "type": "nominal" },
+                  { "field": "Calorie", "type": "nominal" },
+                  { "field": "ONIscore", "type": "nominal" },
+              ],
+              "x": {
+                  "field": "Fat",
+                  "title": "Fat",
+                  "type": "quantitative"
+              },
+              "y": {
+                  "title": "ONIscore",
+                  "field": "ONIscore",
+                  "type": "quantitative"
+              },
+              },
+              "width": 500,
+              "height": 250,
+              "mark": "circle",
+              "params": [
+                  {
+                      "name": "brush",
+                      "select": {
+                      "type": "interval",
+                      "resolve": "union",
+                      "on": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                      "translate": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                      "zoom": "wheel![event.shiftKey]"
+                      }
+                  },
+                  {
+                      "name": "grid",
+                      "select": {
+                          "type": "interval",
+                          "resolve": "global",
+                          "translate": "[mousedown[!event.shiftKey], window:mouseup] > window:mousemove!",
+                          "zoom": "wheel![!event.shiftKey]"
+                      },               
+                      "bind": "scales",
+                  },
+              ],
+              "transform": [{"filter": {"param": "click"}}],
             },
-            //Tooltip for scatterplot
-            "tooltip": [
-                { "field": "foodName", "type": "nominal" },
-                { "field": "Protein", "type": "nominal" },
-                { "field": "Fat", "type": "nominal" },
-                { "field": "Carb", "type": "nominal" },
-                { "field": "Calorie", "type": "nominal" },
-                { "field": "ONIscore", "type": "nominal" },
-            ],
-            "x": {
-                "field": "Protein",
-                "title": "Protein",
-                "type": "quantitative"
-            },
-            "y": {
-                "title": "ONIscore",
-                "field": "val",
-                "type": "quantitative"
-            },
-            "row": {
-              "field": "Zone",
+         {
+          "encoding": {
+          "color": {
+              "condition": {
+              "param": "brush",
+              "title": "Food Categories",
+              "field": "F6",
               "type": "nominal",
-              "title": null
-            }
-            },
-            "mark": "circle",
-            "params": [
-                {
-                    "name": "brush",
-                    "select": {
-                    "type": "interval",
-                    "resolve": "union",
-                    "on": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
-                    "translate": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
-                    "zoom": "wheel![event.shiftKey]"
-                    }
-                },
-                {
-                    "name": "grid",
-                    "select": {
-                        "type": "interval",
-                        "resolve": "global",
-                        "translate": "[mousedown[!event.shiftKey], window:mouseup] > window:mousemove!",
-                        "zoom": "wheel![!event.shiftKey]"
-                    },               
-                    "bind": "scales",
-                },
-            ],
-            "transform": [{"filter": {"param": "click"}}],
 
-        }
-      
-/*         //Scatterplot #2
-
-    {
-      "encoding": {
-      "color": {
-          "condition": {
-          "param": "click",
-          "field": "F6",
+              },
+              "value": "gray"
           },
-          "value": "gray"
-      },
-      "x": {"aggregate": "count", "type": "nominal"},
-      "y": {"title": "Food Category", "field": "F6"}
-      },            
-        //BARCHART 
-        "width": 1000,
-        "mark": "bar",
-        "params": [{
-        "name": "click",
-        "select": {"type": "point", "encodings": ["color"]}
-        }],
-        "transform": [{"filter": {"param": "brush"}}]
-    } */
+          "tooltip": [
+              { "field": "foodName", "type": "nominal" },
+              { "field": "Protein", "type": "nominal" },
+              { "field": "Fat", "type": "nominal" },
+              { "field": "Carb", "type": "nominal" },
+              { "field": "Calorie", "type": "nominal" },
+              { "field": "ONIscore", "type": "nominal" },
+          ],
+          "x": {
+              "field": "Fat",
+              "title": "Fat",
+              "type": "quantitative"
+          },
+          "y": {
+              "title": "ONIscore",
+              "field": "ONIscore",
+              "type": "quantitative"
+          },
+          },
+          "width": 500,
+          "height": 250,
+          "mark": "circle",
+          "params": [
+              {
+                  "name": "brush",
+                  "select": {
+                  "type": "interval",
+                  "resolve": "union",
+                  "on": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                  "translate": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                  "zoom": "wheel![event.shiftKey]"
+                  }
+              },
+              {
+                  "name": "grid",
+                  "select": {
+                      "type": "interval",
+                      "resolve": "global",
+                      "translate": "[mousedown[!event.shiftKey], window:mouseup] > window:mousemove!",
+                      "zoom": "wheel![!event.shiftKey]"
+                  },               
+                  "bind": "scales",
+              },
+          ],
+          "transform": [{"filter": {"param": "click"}}],
 
-/*   "config" : {
-    "concat": {
-      "spacing" : 0,
-      "columns" : 2
-    } */
+        },
+        //Scatterplot #3
+        {
+        "encoding": {
+        "color": {
+            "condition": {
+            "param": "brush",
+            "title": "Food Categories",
+            "field": "F6",
+            "type": "nominal",
+
+            },
+            "value": "gray"
+        },
+        "tooltip": [
+            { "field": "foodName", "type": "nominal" },
+            { "field": "Protein", "type": "nominal" },
+            { "field": "Fat", "type": "nominal" },
+            { "field": "Carb", "type": "nominal" },
+            { "field": "Calorie", "type": "nominal" },
+            { "field": "ONIscore", "type": "nominal" },
+        ],
+        "x": {
+            "field": "Carb",
+            "title": "Carbohydrates",
+            "type": "quantitative"
+        },
+        "y": {
+            "title": "ONIscore",
+            "field": "ONIscore",
+            "type": "quantitative"
+        }
+        },
+        "width": 500,
+        "height": 250, 
+        "mark": "circle",
+        "params": [
+            {
+                "name": "brush",
+                "select": {
+                "type": "interval",
+                "resolve": "union",
+                "on": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                "translate": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                "zoom": "wheel![event.shiftKey]"
+                }
+            },
+            {
+                "name": "grid",
+                "select": {
+                    "type": "interval",
+                    "resolve": "global",
+                    "translate": "[mousedown[!event.shiftKey], window:mouseup] > window:mousemove!",
+                    "zoom": "wheel![!event.shiftKey]"
+                },               
+                "bind": "scales",
+            },
+        ],
+        "transform": [{"filter": {"param": "click"}}],
+
+        },
+        //Scatterplot #4
+        {
+          "encoding": {
+          "color": {
+              "condition": {
+              "param": "brush",
+              "title": "Food Categories",
+              "field": "F6",
+              "type": "nominal",
+    
+              },
+              "value": "gray"
+          },
+          "tooltip": [
+              { "field": "foodName", "type": "nominal" },
+              { "field": "Protein", "type": "nominal" },
+              { "field": "Fat", "type": "nominal" },
+              { "field": "Carb", "type": "nominal" },
+              { "field": "Calorie", "type": "nominal" },
+              { "field": "ONIscore", "type": "nominal" },
+          ],
+          "x": {
+              "field": "Calorie",
+              "title": "Calories pr. 100 grams",
+              "type": "quantitative"
+          },
+          "y": {
+              "title": "ONIscore",
+              "field": "ONIscore",
+              "type": "quantitative"
+          }
+          },
+          "width": 500,
+          "height": 250, 
+          "mark": "circle",
+          "params": [
+              {
+                  "name": "brush",
+                  "select": {
+                  "type": "interval",
+                  "resolve": "union",
+                  "on": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                  "translate": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                  "zoom": "wheel![event.shiftKey]"
+                  }
+              },
+              {
+                  "name": "grid",
+                  "select": {
+                      "type": "interval",
+                      "resolve": "global",
+                      "translate": "[mousedown[!event.shiftKey], window:mouseup] > window:mousemove!",
+                      "zoom": "wheel![!event.shiftKey]"
+                  },               
+                  "bind": "scales",
+              },
+          ],
+          "transform": [{"filter": {"param": "click"}}],
+    
+        },
+        //Scatterplot #5
+        {
+        "encoding": {
+        "color": {
+            "condition": {
+            "param": "brush",
+            "title": "Food Categories",
+            "field": "F6",
+            "type": "nominal",
+    
+            },
+            "value": "gray"
+        },
+        "tooltip": [
+            { "field": "foodName", "type": "nominal" },
+            { "field": "Protein", "type": "nominal" },
+            { "field": "Fat", "type": "nominal" },
+            { "field": "Carb", "type": "nominal" },
+            { "field": "Calorie", "type": "nominal" },
+            { "field": "ONIscore", "type": "nominal" },
+        ],
+        "x": {
+            "field": "Calorie",
+            "title": "Calories pr. 100 grams",
+            "type": "quantitative"
+        },
+        "y": {
+            "title": "ONIscore",
+            "field": "ONIscore",
+            "type": "quantitative"
+        }
+        },
+        "width": 500,
+        "height": 250,
+        "mark": "circle",
+        "params": [
+            {
+                "name": "brush",
+                "select": {
+                "type": "interval",
+                "resolve": "union",
+                "on": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                "translate": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                "zoom": "wheel![event.shiftKey]"
+                }
+            },
+            {
+                "name": "grid",
+                "select": {
+                    "type": "interval",
+                    "resolve": "global",
+                    "translate": "[mousedown[!event.shiftKey], window:mouseup] > window:mousemove!",
+                    "zoom": "wheel![!event.shiftKey]"
+                },               
+                "bind": "scales",
+            },
+        ],
+        "transform": [{"filter": {"param": "click"}}],
+    
+        },
+        //Scatterplot #6
+        {
+        "encoding": {
+        "color": {
+            "condition": {
+            "param": "brush",
+            "title": "Food Categories",
+            "field": "F6",
+            "type": "nominal",
+      
+            },
+            "value": "gray"
+        },
+        "tooltip": [
+            { "field": "foodName", "type": "nominal" },
+            { "field": "Protein", "type": "nominal" },
+            { "field": "Fat", "type": "nominal" },
+            { "field": "Carb", "type": "nominal" },
+            { "field": "Calorie", "type": "nominal" },
+            { "field": "ONIscore", "type": "nominal" },
+        ],
+        "x": {
+            "field": "Calorie",
+            "title": "Calories pr. 100 grams",
+            "type": "quantitative"
+        },
+        "y": {
+            "title": "ONIscore",
+            "field": "ONIscore",
+            "type": "quantitative"
+        }
+        },
+        "width": 500,
+        "height": 250,
+        "mark": "circle",
+        "params": [
+            {
+                "name": "brush",
+                "select": {
+                "type": "interval",
+                "resolve": "union",
+                "on": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                "translate": "[mousedown[event.shiftKey], window:mouseup] > window:mousemove!",
+                "zoom": "wheel![event.shiftKey]"
+                }
+            },
+            {
+                "name": "grid",
+                "select": {
+                    "type": "interval",
+                    "resolve": "global",
+                    "translate": "[mousedown[!event.shiftKey], window:mouseup] > window:mousemove!",
+                    "zoom": "wheel![!event.shiftKey]"
+                },               
+                "bind": "scales",
+            },
+        ],
+        "transform": [{"filter": {"param": "click"}}]
+    },
+        
+        {
+          //Barchart Begin
+          "encoding": {
+          "color": {
+              "condition": {
+              "param": "click",
+              "field": "F6",
+              },
+              "value": "gray"
+          },
+          "x": {"aggregate": "count", "type": "nominal"},
+          "y": {"title": "Food Category", "field": "F6"}
+          },            
+            //BARCHART 
+            "width": 500,
+            "mark": "bar",
+            "params": [{
+            "name": "click",
+            "select": {"type": "point", "encodings": ["color"]}
+            }],
+            "transform": [{"filter": {"param": "brush"}}]
+        }
+        ],
+        "config" : {
+          "concat": {
+         "spacing" : 0,
+         "columns" : 2
+       }
+      } 
+      }
+
 
         
 await vegaEmbed('#scatterPlot', scatterPlot).then(function(result){
